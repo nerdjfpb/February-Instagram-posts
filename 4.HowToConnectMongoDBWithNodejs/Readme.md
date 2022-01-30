c61c2d2b35cf0b800a9d0251e9bc535028ebec45
Muhammad Ali <nerdjfpb@gmail.com>
2022-01-30 23:33:43 +0600

    4. How to connect mongodb with node js

---
+++ 4.HowToConnectMongoDBWithNodejs/Readme.md
@@ -0,0 +1,11 @@
+## To run
+
+- Start with `npm install`
+- Run `npm run dev` to start server
+- Check `localhost:4000/` to see the magic
+
+### Special Note
+
+This is a really demo project! So don't think you can use this code in production! Target audience is totally beginner for backend/nodejs with mongodb!
+
+There are lots of improvement needed for final app. So don't use this code in production!
---
+++ 4.HowToConnectMongoDBWithNodejs/index.js
@@ -0,0 +1,54 @@
+// starts with npm init -y
+// express, cors - npm i express cors
+// nodemon, mongodb  - npm i nodemon mongoose -D
+
+// basic server ----- An screenshot
+
+// mongodb connect with mongoose
+
+// create a todo model
+
+// create an endpoint to add there
+
+// create an endpoint to show all the data there
+
+// finally show the result
+
+// importing libraries
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const port = 4000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// localhost url + db name (connectwithdb)
+mongoose.connect("mongodb://localhost:27017/connectwithdb");
+
+// Model
+const TodoDB = mongoose.model("Todo", { task: String });
+
+// endpoints
+app.get("/", (_req, res) => {
+  // Get all the data and send as response
+  TodoDB.find().then((data) => res.json(data));
+});
+
+app.post("/", (req, res) => {
+  // Get the data from postman
+  const { task } = req.body;
+
+  // create the data for saving in db
+  const singleTodo = new TodoDB({ task: task });
+
+  // save the data in db and send response
+  singleTodo.save().then(() => res.json("New task added"));
+});
+
+// starting server in port (4000)
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
